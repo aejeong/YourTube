52ab7e6a32cc2005b4e79cb80ba3da9d60b6c2db
aejeong <ehejrhc@gmail.com>
2019-01-31 20:41:50 +0900

    deleteVideo

--- YourTube/routers/videoRouter.js
+++ YourTube/routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { videoDetail, editVideo, deleteVideo, getUpload, postUpload } from "../controller/videoController";
+import { videoDetail, deleteVideo, getUpload, postUpload, getEditVideo, postEditVideo } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -9,8 +9,11 @@ videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 
 export default videoRouter;
